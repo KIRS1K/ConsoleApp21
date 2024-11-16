d71cf7262a0dfb3d6db5ec774de4733561aabdd6
KIRS1K <ilya.kirsnov@bk.ru>
2024-11-16 19:37:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.CompilerServices;
+
+abstract class Delivery
+{
+    public abstract void Address();
+    public abstract void Name();
+    public abstract void SecondName();
+    public abstract void PhoneNumber();
+}
+
+class HomeDelivery : Delivery
+{
+    public override void Address()
+    { }
+    public override void Name()
+    { }
+    public override void SecondName()
+    { }
+    public override void PhoneNumber()
+    { }
+}
+
+class PickPointDelivery : Delivery
+{
+    public override void Address()
+    { }
+    public override void Name()
+    { }
+    public override void SecondName()
+    { }
+    public override void PhoneNumber()
+    { }
+}
+
+class ShopDelivery : Delivery
+{
+    public override void Address()
+    { }
+    public override void Name()
+    { }
+    public override void SecondName()
+    { }
+    public override void PhoneNumber()
+    { }
+}
+
+class Order<TDelivery,
+TStruct> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+
+    public int Number;
+
+    public string Description;
+
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+
+    public void DisplayName()
+    {
+        Console.WriteLine(Delivery.Name);
+    }
+
+    public void DisplaySecondName()
+    {
+        Console.WriteLine(Delivery.SecondName);
+    }
+
+    public void DisplayPhoneNumber()
+    {
+        Console.WriteLine(Delivery.PhoneNumber);
+    }
+}    
+
